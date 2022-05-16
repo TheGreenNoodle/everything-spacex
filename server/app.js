be99d339b34d0f5938feff20c8f6df2d31d9105c
afleming089 <aflemingrocks089@gmail.com>
2022-05-16 14:39:34 -0500

    API
    Added some packages for the backends apis.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,12 @@
-const express = require("express");
-
 const PORT = process.env.PORT || 3001;
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
 
 const app = express();
+
+app.use(cors());
 
 app.route("/api").get((req, res) => {
   res.json({ message: "Hi" });
