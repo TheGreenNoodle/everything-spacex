f60e37ffcaf3293d9ac91db7923a2db9ed90c0bc
Anton <aflemingrocks089@gmail.com>
2022-05-19 12:33:32 -0500

    Added some weather data

--- my-app/src/Components/GetData/GetWeather.jsx
+++ my-app/src/Components/GetData/GetWeather.jsx
@@ -19,16 +19,23 @@ function GetWeather(props) {
     axios
       .request(options)
       .then((response) => {
-        setData({ temp: response.data.current.temp });
+        setData({
+          icon:
+            "http://openweathermap.org/img/wn/" +
+            response.data.current.weather[0].icon +
+            "@2x.png",
+          temp: response.data.current.temp,
+          description: response.data.current.weather[0].description,
+          rainChance: response.data.current.rain,
+          visibility: response.data.current.visibility,
+          windSpeed: response.data.current.wind_speed,
+          windDeg: response.data.current.wind_deg,
+        });
       })
       .catch((error) => {
         console.error(error);
       });
   }, []);
-  // const icon = data.current.weather[0].icon;
-  // const imgUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-  const imgUrl =
-    "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcontent.fortune.com%2Fwp-content%2Fuploads%2F2020%2F08%2FGettyImages-1219672105_web.jpg&imgrefurl=https%3A%2F%2Ffortune.com%2Fcompany%2Fspacex%2F&tbnid=X4lIh7GUJ-iAcM&vet=12ahUKEwiNm5H2xOf3AhWbAs0KHcN2BGwQMygAegUIARC8AQ..i&docid=m3gOZlO-lchSaM&w=2880&h=1920&q=space%20x&ved=2ahUKEwiNm5H2xOf3AhWbAs0KHcN2BGwQMygAegUIARC8AQ";
   console.log(data);
   return (
     <div>
@@ -42,7 +49,12 @@ function GetWeather(props) {
           <h1>{props.city}</h1>
           <div>Weather map</div>
           <h1>Todays forecast</h1>
-          <p>Weather icon Todays forcast</p>
+          <img src={data.icon} alt="weather-icon" />
+          <p>Temp {data.temp}</p>
+          <p>There is {data.description}.</p>
+          <p>Visibility {data.visibility}</p>
+          <p>Wind speed {data.windSpeed}</p>
+          <p>Wind deg {data.windDeg}</p>
           <h1>7 day forecast</h1>
         </div>
       )}
